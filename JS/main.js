6c771bd52e57a4eab9cfb4b88993780d87320596
Yury Martins <mandapara@yury.com.br>
2022-04-30 09:36:30 -0300

    Adição de banner principal

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-// muda cor de fundo ao scroll //
+// Muda cor de fundo ao scroll 
 
 function changeBg(){
     var navbar = document.getElementById('navbar')
@@ -12,3 +12,15 @@ function changeBg(){
 }
 
 window.addEventListener('scroll', changeBg);
+
+// Hamburger Menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
