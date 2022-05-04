a30127a4bc2f0964d6c43bbc67ff194174dfd776
Yury Martins <mandapara@yury.com.br>
2022-05-03 23:40:26 -0300

    Seção Sobre Mim e animações

--- JS/main.js
+++ JS/main.js
@@ -97,3 +97,22 @@ function Delete() {
 
 // Iniciar o efeito de digitação ao carregar
 _INTERVAL_VAL = setInterval(Type, 200);
+
+// Elementos se revelam ao scroll
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+	var reveals = document.querySelectorAll('.reveal');
+
+	for(var i = 0; i < reveals.length; i++){
+
+		var windowheight = window.innerHeight;
+		var revealtop = reveals[i].getBoundingClientRect().top;
+		var revealpoint = 150;
+
+		if(revealtop < windowheight - revealpoint){
+			reveals[i].classList.add('active');
+		}
+	}
+}
